e9ad2f88d07cc4010bfcfab88b8cfe1504a142a2
Maxime Orione <maxpower98@hotmail.com>
2020-01-03 21:42:52 +0100

    ♻ Moved collision detection to Ball class

--- Ball.ts
+++ Ball.ts
@@ -1,8 +1,9 @@
-import { FPS, GRAVITY_ACCELERATION } from './config';
+import { COLLISION_COEFFICIENT, FPS, GRAVITY_ACCELERATION } from './config';
 import { getAccelerationX, getAccelerationY, getAerodynamicForce } from './physicsUtil';
 import Point from './Point';
 
 export default class Ball {
+	public readonly id: number;
 	public readonly radius = 10;
 	public readonly color = 'blue';
 	public readonly mass = 10;
@@ -13,8 +14,9 @@ export default class Ball {
 		return (Math.PI * this.radius * this.radius) / 1000;
 	}
 
-	constructor(position: Point) {
+	constructor(position: Point, id: number) {
 		this.position = { ...position };
+		this.id = id;
 	}
 
 	public tick(drag: number, density: number, gravity: number): void {
@@ -29,5 +31,65 @@ export default class Ball {
 
 		this.position.x += this.velocity.x * FPS * 100;
 		this.position.y += this.velocity.y * FPS * 100;
+	}
+
+	public collisionWall(width: number, height: number): void {
+		if (this.position.x > width - this.radius) {
+			this.velocity.x *= COLLISION_COEFFICIENT;
+			this.position.x = width - this.radius;
+		}
+		if (this.position.y > height - this.radius) {
+			this.velocity.y *= COLLISION_COEFFICIENT;
+			this.position.y = height - this.radius;
+		}
+		if (this.position.x < this.radius) {
+			this.velocity.x *= COLLISION_COEFFICIENT;
+			this.position.x = this.radius;
+		}
+		if (this.position.y < this.radius) {
+			this.velocity.y *= COLLISION_COEFFICIENT;
+			this.position.y = this.radius;
+		}
+	}
+
+	public collisionOtherBalls(otherBalls: Array<Ball>): void {
+		for (let otherBall of otherBalls) {
+			if (this.id !== otherBall.id) {
+				// quick check for potential collisions using AABBs
+				if (this.position.x + this.radius + otherBall.radius > otherBall.position.x
+					&& this.position.x < otherBall.position.x + this.radius + otherBall.radius
+					&& this.position.y + this.radius + otherBall.radius > otherBall.position.y
+					&& this.position.y < otherBall.position.y + this.radius + otherBall.radius) {
+
+					// pythagoras
+					const distX = this.position.x - otherBall.position.x;
+					const distY = this.position.y - otherBall.position.y;
+					const d = Math.sqrt((distX) * (distX) + (distY) * (distY));
+
+					// checking circle vs circle collision
+					if (d < this.radius + otherBall.radius) {
+						const nx = (otherBall.position.x - this.position.x) / d;
+						const ny = (otherBall.position.y - this.position.y) / d;
+						const p = 2 * (this.velocity.x * nx + this.velocity.y * ny - otherBall.velocity.x * nx - otherBall.velocity.y * ny) / (this.mass + otherBall.mass);
+
+						// calculating the point of collision
+						const colPointX = ((this.position.x * otherBall.radius) + (otherBall.position.x * this.radius)) / (this.radius + otherBall.radius);
+						const colPointY = ((this.position.y * otherBall.radius) + (otherBall.position.y * this.radius)) / (this.radius + otherBall.radius);
+
+						// stopping overlap
+						this.position.x = colPointX + this.radius * (this.position.x - otherBall.position.x) / d;
+						this.position.y = colPointY + this.radius * (this.position.y - otherBall.position.y) / d;
+						otherBall.position.x = colPointX + otherBall.radius * (otherBall.position.x - this.position.x) / d;
+						otherBall.position.y = colPointY + otherBall.radius * (otherBall.position.y - this.position.y) / d;
+
+						// updating velocity to reflect collision
+						this.velocity.x -= p * this.mass * nx;
+						this.velocity.y -= p * this.mass * ny;
+						otherBall.velocity.x += p * otherBall.mass * nx;
+						otherBall.velocity.y += p * otherBall.mass * ny;
+					}
+				}
+			}
+		}
 	}
 }
--- BallApp.ts
+++ BallApp.ts
@@ -1,5 +1,5 @@
 import Ball from './Ball';
-import { COLLISION_COEFFICIENT, GRAVITY_ACCELERATION, TIMER_SPEED } from './config';
+import { GRAVITY_ACCELERATION, TIMER_SPEED } from './config';
 import Point from './Point';
 
 class Mouse extends Point {
@@ -7,6 +7,7 @@ class Mouse extends Point {
 	public y = 0;
 	public isDown = false;
 }
+
 export default class BallApp {
 	private readonly canvas: HTMLCanvasElement;
 	private readonly ctx: CanvasRenderingContext2D;
@@ -16,6 +17,7 @@ export default class BallApp {
 	private timerID: number;
 	private mouse = new Mouse();
 	private balls: Array<Ball> = [];
+
 	public constructor() {
 		this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
 		this.ctx = this.canvas.getContext('2d');
@@ -23,12 +25,14 @@ export default class BallApp {
 		this.densityElement = document.getElementById('density') as HTMLInputElement;
 		this.dragElement = document.getElementById('drag') as HTMLInputElement;
 	}
+
 	public start(): void {
 		this.canvas.addEventListener('mousedown', this.mouseDown.bind(this));
 		this.canvas.addEventListener('mouseup', this.mouseUp.bind(this));
 		this.canvas.addEventListener('mousemove', this.setMousePositionForEvent.bind(this));
 		this.timerID = setInterval(this.loop.bind(this), TIMER_SPEED);
 	}
+
 	private loop(): void {
 		this.ctx.clearRect(0, 0, this.width, this.height);
 		for (let i = 0; i < this.balls.length; i++) {
@@ -36,76 +40,25 @@ export default class BallApp {
 			if (!this.mouse.isDown || i < this.balls.length - 1) {
 				ball.tick(this.drag, this.density, this.gravity);
 			}
-			this.collisionOtherBalls(ball);
-			this.collisionWall(ball);
+			ball.collisionOtherBalls(this.balls);
+			ball.collisionWall(this.width, this.height);
 		}
 		this.render();
 	}
-	private collisionOtherBalls(ball: Ball): void {
-		for (let i = 0; i < this.balls.length; i++) {
-			const otherBall = this.balls[i];
-			if (ball.position.x !== otherBall.position.x && ball.position.y !== otherBall.position.y) {
-				// quick check for potential collisions using AABBs
-				if (ball.position.x + ball.radius + otherBall.radius > otherBall.position.x
-					&& ball.position.x < otherBall.position.x + ball.radius + otherBall.radius
-					&& ball.position.y + ball.radius + otherBall.radius > otherBall.position.y
-					&& ball.position.y < otherBall.position.y + ball.radius + otherBall.radius) {
-					// pythagoras
-					const distX = ball.position.x - otherBall.position.x;
-					const distY = ball.position.y - otherBall.position.y;
-					const d = Math.sqrt((distX) * (distX) + (distY) * (distY));
-					// checking circle vs circle collision
-					if (d < ball.radius + otherBall.radius) {
-						const nx = (otherBall.position.x - ball.position.x) / d;
-						const ny = (otherBall.position.y - ball.position.y) / d;
-						const p = 2 * (ball.velocity.x * nx + ball.velocity.y * ny - otherBall.velocity.x * nx - otherBall.velocity.y * ny) / (ball.mass + otherBall.mass);
-						// calculating the point of collision
-						const colPointX = ((ball.position.x * otherBall.radius) + (otherBall.position.x * ball.radius)) / (ball.radius + otherBall.radius);
-						const colPointY = ((ball.position.y * otherBall.radius) + (otherBall.position.y * ball.radius)) / (ball.radius + otherBall.radius);
-						// stopping overlap
-						ball.position.x = colPointX + ball.radius * (ball.position.x - otherBall.position.x) / d;
-						ball.position.y = colPointY + ball.radius * (ball.position.y - otherBall.position.y) / d;
-						otherBall.position.x = colPointX + otherBall.radius * (otherBall.position.x - ball.position.x) / d;
-						otherBall.position.y = colPointY + otherBall.radius * (otherBall.position.y - ball.position.y) / d;
-						// updating velocity to reflect collision
-						ball.velocity.x -= p * ball.mass * nx;
-						ball.velocity.y -= p * ball.mass * ny;
-						otherBall.velocity.x += p * otherBall.mass * nx;
-						otherBall.velocity.y += p * otherBall.mass * ny;
-					}
-				}
-			}
-		}
-	}
-	private collisionWall(ball: Ball): void {
-		if (ball.position.x > this.width - ball.radius) {
-			ball.velocity.x *= COLLISION_COEFFICIENT;
-			ball.position.x = this.width - ball.radius;
-		}
-		if (ball.position.y > this.height - ball.radius) {
-			ball.velocity.y *= COLLISION_COEFFICIENT;
-			ball.position.y = this.height - ball.radius;
-		}
-		if (ball.position.x < ball.radius) {
-			ball.velocity.x *= COLLISION_COEFFICIENT;
-			ball.position.x = ball.radius;
-		}
-		if (ball.position.y < ball.radius) {
-			ball.velocity.y *= COLLISION_COEFFICIENT;
-			ball.position.y = ball.radius;
-		}
-	}
+
 	private mouseDown(event: MouseEvent): void {
 		if (event.button === 0) {
 			this.setMousePositionForEvent(event);
 			this.mouse.isDown = true;
-			this.balls.push(new Ball(this.mouse));
+			this.balls.push(new Ball(this.mouse, this.balls.length));
 		}
 	}
+
 	private setMousePositionForEvent(event: MouseEvent): void {
 		this.mouse.x = event.pageX - this.canvas.offsetLeft;
 		this.mouse.y = event.pageY - this.canvas.offsetTop;
 	}
+
 	private mouseUp(event: MouseEvent): void {
 		if (event.button === 0) {
 			this.mouse.isDown = false;
@@ -113,6 +66,7 @@ export default class BallApp {
 			this.lastBall.velocity.y = (this.lastBall.position.y - this.mouse.y) / 10;
 		}
 	}
+
 	private render(): void {
 		this.renderBalls();
 		if (this.mouse.isDown) {
@@ -120,9 +74,11 @@ export default class BallApp {
 		}
 		this.renderInfo();
 	}
+
 	private renderBalls(): void {
 		this.balls.forEach(this.renderBall.bind(this));
 	}
+
 	private renderBall(ball: Ball): void {
 		this.ctx.beginPath();
 		this.ctx.fillStyle = ball.color;
@@ -130,6 +86,7 @@ export default class BallApp {
 		this.ctx.fill();
 		this.ctx.closePath();
 	}
+
 	private renderSlingshot(): void {
 		this.ctx.beginPath();
 		this.ctx.strokeStyle = 'red';
@@ -138,6 +95,7 @@ export default class BallApp {
 		this.ctx.stroke();
 		this.ctx.closePath();
 	}
+
 	private renderInfo(): void {
 		this.ctx.fillStyle = 'black';
 		this.ctx.font = '11pt monospace';
@@ -147,6 +105,7 @@ export default class BallApp {
 		this.ctx.fillText(`Acceleration due to gravity: ${this.gravity * GRAVITY_ACCELERATION} g`, 0, 64);
 		this.ctx.fillText(`Mouse: ${this.mouse.isDown}`, 0, 80);
 	}
+
 	private get lastBall(): Ball {
 		return this.balls[this.balls.length - 1];
 	}
